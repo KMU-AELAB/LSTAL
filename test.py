b20f3914b3941abf6648f696fe7450e6417089dd
dollking <dollking@kookmin.ac.kr>
2021-11-14 21:25:29 +0900

    add LSTAL

---
+++ test.py
@@ -0,0 +1,305 @@
+import os
+import random
+
+# Torch
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.data.sampler import SubsetRandomSampler
+
+# Torchvison
+from torchvision.datasets import CIFAR100, CIFAR10
+
+# Utils
+from tqdm import tqdm
+
+# Custom
+from config import *
+from data.transform import Cifar
+
+import models.resnet as resnet
+import models.vae as vae
+import models.featurenet as featurenet
+
+from data.sampler import SubsetSequentialSampler
+
+
+# Seed
+random.seed('KMU_AELAB')
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+
+
+# Data
+transforms = Cifar()
+
+if DATASET == 'cifar10':
+    data_train = CIFAR10('./data', train=True, download=True, transform=transforms.train_transform)
+    data_unlabeled = CIFAR10('./data', train=True, download=True, transform=transforms.test_transform)
+    data_test = CIFAR10('./data', train=False, download=True, transform=transforms.test_transform)
+elif DATASET == 'cifar100':
+    data_train = CIFAR100('./data', train=True, download=True, transform=transforms.train_transform)
+    data_unlabeled = CIFAR100('./data', train=True, download=True, transform=transforms.test_transform)
+    data_test = CIFAR100('./data', train=False, download=True, transform=transforms.test_transform)
+
+
+# Train Utils
+iters = 0
+
+
+def cosine_distance(feature1, feature2):
+    return 1 - nn.CosineSimilarity(feature1, feature2)
+
+
+def additional_train(models, optimizers, dataloaders):
+    models['vae'].eval()
+    models['backbone'].eval()
+    models['module'].train()
+    global iters
+
+    for data in tqdm(dataloaders['train'], leave=False, total=len(dataloaders['train'])):
+        inputs = data[0].cuda()
+        iters += 1
+
+        optimizers['module'].zero_grad()
+
+        _, features = models['backbone'](inputs)
+
+        features[0] = features[0].detach()
+        features[1] = features[1].detach()
+        features[2] = features[2].detach()
+        features[3] = features[3].detach()
+
+        pred_feature = models['module'](features)
+        pred_feature = pred_feature.view([-1, EMBEDDING_DIM])
+        _, target_feature, _, _ = models['vae'](inputs)
+
+        loss = torch.mean(cosine_distance(pred_feature, target_feature.detach()))
+
+        loss.backward()
+        optimizers['module'].step()
+
+
+def train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss):
+    models['backbone'].train()
+    models['module'].train()
+    models['vae'].eval()
+    global iters
+
+    _weight = WEIGHT
+    for data in tqdm(dataloaders['train'], leave=False, total=len(dataloaders['train'])):
+        inputs = data[0].cuda()
+        labels = data[1].cuda()
+        iters += 1
+
+        optimizers['backbone'].zero_grad()
+        optimizers['module'].zero_grad()
+
+        scores, features = models['backbone'](inputs)
+        target_loss = criterion(scores, labels)
+
+        if epoch > epoch_loss:
+            _weight = 0.001
+            features[0] = features[0].detach()
+            features[1] = features[1].detach()
+            features[2] = features[2].detach()
+            features[3] = features[3].detach()
+
+        pred_feature = models['module'](features)
+        pred_feature = pred_feature.view([-1, EMBEDDING_DIM])
+        _, target_feature, _, _ = models['vae'](inputs)
+
+        m_backbone_loss = torch.sum(target_loss) / target_loss.size(0)
+        m_module_loss = torch.mean(cosine_distance(pred_feature, target_feature.detach()))
+        loss = m_backbone_loss + _weight * m_module_loss
+
+        loss.backward()
+        optimizers['backbone'].step()
+        optimizers['module'].step()
+
+
+def test(models, dataloaders, mode='val'):
+    assert mode == 'val' or mode == 'test'
+    models['backbone'].eval()
+    models['module'].eval()
+
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for (inputs, labels) in dataloaders[mode]:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+
+            scores, _ = models['backbone'](inputs)
+            _, preds = torch.max(scores.data, 1)
+            total += labels.size(0)
+            correct += (preds == labels).sum().item()
+    
+    return 100 * correct / total
+
+
+def train(models, criterion, optimizers, schedulers, dataloaders, num_epochs, epoch_loss):
+    print('>> Train a Model.')
+    best_acc = 0.
+    checkpoint_dir = os.path.join(f'./{DATASET}', 'train', 'weights')
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    
+    for epoch in range(num_epochs):
+        schedulers['backbone'].step()
+        schedulers['module'].step()
+
+        train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss)
+
+        # Save a checkpoint
+        if False and epoch % 5 == 4:
+            acc = test(models, dataloaders, 'test')
+            if best_acc < acc:
+                best_acc = acc
+                torch.save(
+                    {
+                    'epoch': epoch + 1,
+                    'state_dict_backbone': models['backbone'].state_dict(),
+                    'state_dict_module': models['module'].state_dict()
+                    },
+                    f'{checkpoint_dir}/active_resnet18_{DATASET}.pth'
+                )
+            print('Val Acc: {:.3f} \t Best Acc: {:.3f}'.format(acc, best_acc))
+
+    for epoch in range(num_epochs, num_epochs + (num_epochs // 2)):
+        schedulers['module'].step()
+
+        additional_train(models, optimizers, dataloaders)
+
+        # Save a checkpoint
+        if False and epoch % 5 == 4:
+            acc = test(models, dataloaders, 'test')
+            if best_acc < acc:
+                best_acc = acc
+                torch.save(
+                    {
+                    'epoch': epoch + 1,
+                    'state_dict_backbone': models['backbone'].state_dict(),
+                    'state_dict_module': models['module'].state_dict()
+                    },
+                    f'{checkpoint_dir}/active_resnet18_{DATASET}.pth'
+                )
+            print('Val Acc: {:.3f} \t Best Acc: {:.3f}'.format(acc, best_acc))
+
+    print('>> Finished.')
+
+
+def get_uncertainty(models, unlabeled_loader):
+    models['backbone'].eval()
+    models['module'].eval()
+    models['vae'].eval()
+
+    uncertainty = torch.tensor([]).cuda()
+    with torch.no_grad():
+        for (inputs, labels) in unlabeled_loader:
+            inputs = inputs.cuda()
+
+            _, features = models['backbone'](inputs)
+            pred_feature = models['module'](features)
+            pred_feature = pred_feature.view([-1, EMBEDDING_DIM])
+
+            _, target_feature, _, _ = models['vae'](inputs)
+
+            loss = cosine_distance(pred_feature, target_feature.detach())
+
+            uncertainty = torch.cat((uncertainty, loss), 0)
+    
+    return uncertainty.cpu()
+
+
+# Main
+if __name__ == '__main__':
+    for trial in range(TRIALS):
+        fp = open(f'record_{trial + 1}.txt', 'w')
+
+        indices = list(range(NUM_TRAIN))
+        random.shuffle(indices)
+        labeled_set = indices[:INITC]
+        unlabeled_set = indices[INITC:]
+
+        train_loader = DataLoader(data_train, batch_size=BATCH,
+                                  sampler=SubsetRandomSampler(labeled_set),
+                                  pin_memory=True)
+        test_loader = DataLoader(data_test, batch_size=BATCH)
+        dataloaders = {'train': train_loader, 'test': test_loader}
+
+        # Model
+        resnet18 = resnet.ResNet18(num_classes=10).cuda()
+        feature_module = featurenet.FeatureNet(out_dim=EMBEDDING_DIM).cuda()
+
+        target_module = vae.VAE(NUM_RESIDUAL_LAYERS, NUM_RESIDUAL_HIDDENS, EMBEDDING_DIM)
+        checkpoint = torch.load(f'vae/ae_{trial + 1}.pth.tar')
+        target_module.load_state_dict(checkpoint['ae_state_dict'])
+        target_module.cuda()
+
+        models = {'backbone': resnet18, 'module': feature_module, 'vae': target_module}
+
+        torch.backends.cudnn.benchmark = False
+
+        # Active learning cycles
+        for cycle in range(CYCLES):
+            # Loss, criterion and scheduler (re)initialization
+            criterion = nn.CrossEntropyLoss(reduction='none')
+            optim_backbone = optim.SGD(models['backbone'].parameters(), lr=LR,
+                                       momentum=MOMENTUM, weight_decay=WDECAY)
+            optim_module = optim.SGD(models['module'].parameters(), lr=LR,
+                                     momentum=MOMENTUM, weight_decay=WDECAY)
+            sched_backbone = lr_scheduler.MultiStepLR(optim_backbone, milestones=MILESTONES)
+            sched_module = lr_scheduler.MultiStepLR(optim_module, milestones=MILESTONES)
+
+            optimizers = {'backbone': optim_backbone, 'module': optim_module}
+            schedulers = {'backbone': sched_backbone, 'module': sched_module}
+
+            # Training and test
+            train(models, criterion, optimizers, schedulers, dataloaders, EPOCH, EPOCHL)
+            acc = test(models, dataloaders, mode='test')
+
+            fp.write(f'{acc}\n')
+            print('Trial {}/{} || Cycle {}/{} || Label set size {}: Test acc {}'.format(trial + 1, TRIALS, cycle + 1,
+                                                                                        CYCLES, len(labeled_set), acc))
+
+            ##
+            #  Update the labeled dataset via loss prediction-based uncertainty measurement
+
+            # Randomly sample 10000 unlabeled data points
+            random.shuffle(unlabeled_set)
+            subset = unlabeled_set[:SUBSET]
+
+            # Create unlabeled dataloader for the unlabeled subset
+            unlabeled_loader = DataLoader(data_unlabeled, batch_size=BATCH,
+                                          sampler=SubsetSequentialSampler(subset),
+                                          pin_memory=True)
+
+            # Measure uncertainty of each data points in the subset
+            uncertainty = get_uncertainty(models, unlabeled_loader)
+
+            # Index in ascending order
+            arg = np.argsort(uncertainty)
+
+            # Update the labeled dataset and the unlabeled dataset, respectively
+            labeled_set += list(torch.tensor(subset)[arg][-ADDENDUM:].numpy())
+            unlabeled_set = list(torch.tensor(subset)[arg][:-ADDENDUM].numpy()) + unlabeled_set[SUBSET:]
+
+            # Create a new dataloader for the updated labeled dataset
+            dataloaders['train'] = DataLoader(data_train, batch_size=BATCH,
+                                              sampler=SubsetRandomSampler(labeled_set),
+                                              pin_memory=True)
+
+        # Save a checkpoint
+        fp.close()
+        torch.save(
+            {
+            'trial': trial + 1,
+            'state_dict_backbone': models['backbone'].state_dict(),
+            'state_dict_module': models['module'].state_dict()
+            },
+            f'./{DATASET}/train/weights/active_resnet18_{DATASET}_trial{trial}.pth'
+        )
