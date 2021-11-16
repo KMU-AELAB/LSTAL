e20a8b57041a65e038a616c350515c38c5db9404
dollking <dollking@kookmin.ac.kr>
2021-11-16 13:00:52 +0900

    add autoencoder trainer

---
+++ autoencoder/transform.py
@@ -0,0 +1,9 @@
+import torchvision.transforms as T
+
+
+class Cifar(object):
+    def __init__(self):
+        self.transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        ])
---
+++ autoencoder/vae.py
@@ -0,0 +1,146 @@
+'''Active Learning Procedure in PyTorch.
+
+Reference:
+[Yoo et al. 2019] Learning Loss for Active Learning (https://arxiv.org/abs/1905.03677)
+'''
+
+# Python
+import os
+import random
+
+# Torch
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.data.sampler import SubsetRandomSampler
+
+# Torchvison
+from torchvision.datasets import CIFAR100, CIFAR10
+
+# Utils
+from tqdm import tqdm
+
+# Custom
+from models.vae import VAE
+from config import *
+from transform import Cifar
+
+# Seed
+random.seed('KMU_AELAB')
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+
+##
+# Data
+transforms = Cifar()
+
+if DATASET == 'cifar10':
+    data_train = CIFAR10('./data', train=True, download=True, transform=transforms.transform)
+    data_unlabeled = CIFAR10('./data', train=True, download=True, transform=transforms.transform)
+    data_test = CIFAR10('./data', train=False, download=True, transform=transforms.transform)
+elif DATASET == 'cifar100':
+    data_train = CIFAR100('./data', train=True, download=True, transform=transforms.transform)
+    data_unlabeled = CIFAR100('./data', train=True, download=True, transform=transforms.transform)
+    data_test = CIFAR100('./data', train=False, download=True, transform=transforms.transform)
+
+
+# Train Utils
+iters = 0
+
+
+def train_epoch(model, criterion, opt, dataloaders):
+    model.train()
+    global iters
+
+    cnt = 0
+    _loss = 0.
+    for data in tqdm(dataloaders['train'], leave=False, total=len(dataloaders['train'])):
+        cnt += 1
+        iters += 1
+
+        opt.zero_grad()
+        inputs = data[0].cuda()
+
+        recon, features, mu, logvar = model(inputs)
+        recon_loss = criterion(recon, inputs)
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim=1), dim=0)
+
+        loss = recon_loss + (0.001 * kld_loss)
+
+        _loss += loss
+
+        loss.backward()
+        opt.step()
+
+    return _loss / cnt
+
+
+def test(models, dataloaders, mode='val'):
+    assert mode == 'val' or mode == 'test'
+    models['backbone'].eval()
+    models['module'].eval()
+
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for (inputs, labels) in dataloaders[mode]:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+
+            scores, _ = models['backbone'](inputs)
+            _, preds = torch.max(scores.data, 1)
+            total += labels.size(0)
+            correct += (preds == labels).sum().item()
+    
+    return 100 * correct / total
+
+
+def train(model, criterion, opt, scheduler, dataloaders, num_epochs, trial):
+    print('>> Train a Model.')
+    best_acc = 0.
+    checkpoint_dir = os.path.join(f'./{DATASET}', 'train', 'weights')
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    
+    for epoch in range(num_epochs):
+        loss = train_epoch(model, criterion, opt, dataloaders)
+        scheduler.step(loss)
+
+        # Save a checkpoint
+        if epoch % 5 == 4:
+            acc = test(model, dataloaders, 'test')
+            if best_acc < acc:
+                best_acc = acc
+                torch.save(
+                    {
+                        'epoch': epoch + 1,
+                        'ae_state_dict': model.state_dict(),
+                    },
+                    f'../trained_vae/ae_{trial}.pth'
+                )
+            print('Val Acc: {:.3f} \t Best Acc: {:.3f}'.format(acc, best_acc))
+    print('>> Finished.')
+
+
+# Main
+if __name__ == '__main__':
+    for trial in range(TRIALS):
+        train_loader = DataLoader(data_train, batch_size=BATCH, pin_memory=True)
+        test_loader = DataLoader(data_test, batch_size=BATCH)
+        dataloaders = {'train': train_loader, 'test': test_loader}
+
+        # Model
+        model = VAE(NUM_RESIDUAL_LAYERS, NUM_RESIDUAL_HIDDENS, EMBEDDING_DIM).cuda()
+        torch.backends.cudnn.benchmark = False
+
+        # Loss, criterion and scheduler (re)initialization
+        criterion = nn.MSELoss()
+        opt = optim.Adam(model.parameters(), lr=LR)
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', factor=0.8, cooldown=4)
+
+        # Training and test
+        train(model, criterion, opt, scheduler, dataloaders, EPOCH, trial)
